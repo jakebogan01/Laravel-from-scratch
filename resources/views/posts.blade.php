5b2d6f399297c06dc2f1051ed29f551335a6baae
Jake Bogan <jake@Jakes-iMac.local>
2023-06-26 12:41:34 -0400

    make a route and link to it

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>My Blog</title>
+        <link rel="stylesheet" href="/app.css">
+    </head>
+    <body>
+        <article>
+            <h1><a href="/post">My First Post</a></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos similique iusto fuga, laborum architecto temporibus, corrupti expedita nihil voluptate modi obcaecati libero recusandae? Suscipit quo exercitationem placeat molestiae eveniet enim in quisquam error et sint facere officia debitis quod modi obcaecati saepe culpa corrupti consectetur veniam fugiat dicta, harum laboriosam eius nemo. Sed, nesciunt illo? Consequuntur est aperiam doloribus omnis consequatur. Adipisci voluptatum, quia vero facilis quae ut aut perferendis est iste! Veritatis quos eum ipsam? Dolore voluptate quae beatae! Voluptas, qui, voluptatem esse rerum voluptatum praesentium iure labore libero a inventore fugiat, recusandae asperiores doloribus repudiandae odit sequi fugit!</p>
+        </article>
+
+        <article>
+            <h1><a href="/post">My Second Post</a></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos similique iusto fuga, laborum architecto temporibus, corrupti expedita nihil voluptate modi obcaecati libero recusandae? Suscipit quo exercitationem placeat molestiae eveniet enim in quisquam error et sint facere officia debitis quod modi obcaecati saepe culpa corrupti consectetur veniam fugiat dicta, harum laboriosam eius nemo. Sed, nesciunt illo? Consequuntur est aperiam doloribus omnis consequatur. Adipisci voluptatum, quia vero facilis quae ut aut perferendis est iste! Veritatis quos eum ipsam? Dolore voluptate quae beatae! Voluptas, qui, voluptatem esse rerum voluptatum praesentium iure labore libero a inventore fugiat, recusandae asperiores doloribus repudiandae odit sequi fugit!</p>
+        </article>
+        
+        <article>
+            <h1><a href="/post">My Third Post</a></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos similique iusto fuga, laborum architecto temporibus, corrupti expedita nihil voluptate modi obcaecati libero recusandae? Suscipit quo exercitationem placeat molestiae eveniet enim in quisquam error et sint facere officia debitis quod modi obcaecati saepe culpa corrupti consectetur veniam fugiat dicta, harum laboriosam eius nemo. Sed, nesciunt illo? Consequuntur est aperiam doloribus omnis consequatur. Adipisci voluptatum, quia vero facilis quae ut aut perferendis est iste! Veritatis quos eum ipsam? Dolore voluptate quae beatae! Voluptas, qui, voluptatem esse rerum voluptatum praesentium iure labore libero a inventore fugiat, recusandae asperiores doloribus repudiandae odit sequi fugit!</p>
+        </article>
+    </body>
+</html>
