c1441ebbd2a278e04d19cfaff445bbf70800a347
Jake Bogan <jakebogan01@gmail.com>
2023-07-10 15:02:13 -0400

    collection sorting and caching refresher

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,33 +6,22 @@ use Illuminate\Support\Facades\File;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post {
-    public $title;
-    public $excerpt;
-    public $date;
-    public $body;
-    public $slug;
-
-    public function __construct($title, $excerpt, $date, $body, $slug) {
-        $this->title = $title;
-        $this->excerpt = $excerpt;
-        $this->date = $date;
-        $this->body = $body;
-        $this->slug = $slug;
-    }
+    public function __construct(public $title, public $excerpt, public $date, public $body, public $slug) {}
 
     public static function all() {
         // get all files in posts directory and place into a collection
-        return collect(File::files(resource_path("posts")))
-        // loop through each file and parse the yaml front matter document
-        ->map(fn($file) => YamlFrontMatter::parseFile($file))
-        // loop through each document and create a new Post object
-        ->map(fn($document) => new Post(
-                $document->title,
-                $document->excerpt,
-                $document->date,
-                $document->body(),
-                $document->slug
-            ));
+        return cache()->rememberForever('posts.all', function() {
+            return collect(File::files(resource_path("posts")))
+            ->map(fn($file) => YamlFrontMatter::parseFile($file))
+            ->map(fn($document) => new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+                ))
+            ->sortByDesc('date');
+        });
     }
 
     public static function find($slug) {
