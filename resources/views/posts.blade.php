0eea88e491696ddc799c24a1902b31bb81307f2d
Jake Bogan <jakebogan01@gmail.com>
2023-07-25 14:06:23 -0400

    blade layouts two ways

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>My Blog</title>
-        <link rel="stylesheet" href="/app.css">
-    </head>
-    <body>
-        @foreach ($posts as $post)
+<x-layout>
+    @foreach($posts as $post)
         {{--access foreach loop variable--}}
         {{--@dd($loop)--}}
         {{--example: class="{{ $loop->even ?: 'foo' }}"--}}
@@ -23,6 +14,6 @@
                     {{ $post->excerpt }}
                 </div>
             </article>
-        @endforeach
-    </body>
-</html>
+    @endforeach
+</x-layout>
+
