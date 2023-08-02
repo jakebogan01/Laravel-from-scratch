9a7caba777bb74ed52902cad7556db53afb49f2a
Jake Bogan <jakebogan01@gmail.com>
2023-08-02 15:27:14 -0400

    quick tweaks and clean-Up

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -5,7 +5,7 @@
         {{ $trigger }}
     </div>
 
-    <div x-show="show" class="absolute bg-gray-100 rounded-xl w-full mt-2 text-left text-sm leading-6 py-2 z-50" style="display: none;">
+    <div x-show="show" class="absolute bg-gray-100 rounded-xl w-full mt-2 text-left text-sm leading-6 py-2 z-50 overflow-auto max-h-52" style="display: none;">
         {{ $slot }}
     </div>
 </div>
