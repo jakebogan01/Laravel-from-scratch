10423232fcb99a44a03344a3918a09a1e921dcb2
Jake Bogan <jakebogan01@gmail.com>
2023-08-07 17:47:48 -0400

    mailchimp api tinkering

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('ping', function () {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us12'
+    ]);
+
+    $response = $mailchimp->lists->getAllLists();
+
+    dd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 Route::post('/posts/{post:slug}/comments', [CommentController::class, 'store']);
