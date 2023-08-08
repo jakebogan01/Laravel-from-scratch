b5387a6d1ca0708c4fefc5461ad66e1e804df8c7
Jake Bogan <jakebogan01@gmail.com>
2023-08-08 12:38:29 -0400

    limit access to only admins

---
+++ app/Http/Middleware/MustBeAdministrator.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class MustBeAdministrator
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // if the user is not jake then the route is protected
+        if (auth()->user()?->username !== 'Jake') {
+            abort(403);
+        }
+
+        return $next($request);
+    }
+}
